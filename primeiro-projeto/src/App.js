d9d3391e1f1a733694733453365caa633802c27c
Your Name <you@example.com>
2020-12-15 11:09:24 -0300

    criaçao projeto crud

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+/*class App extends React.Component{
+
+state = {
+  nome: '',
+
 }
+
+modificarNome = (e) => {
+  let nome = e.target.value;
+  this.setState({
+    nome: nome
+  })
+}
+criaComboBox = () =>{
+  const opcoes = ['Fulano', 'Ciclano'];
+  const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+    return(
+      <select>{comboBoxOpcoes}</select>
+    )
+
+}
+
+  componentDidMount(){
+    console.log('Executou o Componente didmount');
+  }
+  render(){
+    console.log('Executou o Componente Render');
+
+    const MeuComboBox = () =>  this.criaComboBox();
+
+    return(
+
+      <>
+      <input type="text" value={this.state.nome} onChange={this.modificarNome}></input>
+      <h1>Hello {this.props.nome} e tenho {this.props.idade} anos </h1>
+      <MeuComboBox />
+      </>
+    )
+  }
+}
+*/
+
+
+
+function App(props){
+
+
+const modificaNome = (e) => {console.log(e.target.value)};
+
+
+
+const criaComboBox = () =>{
+  const opcoes = ['Fulano', 'Ciclano'];
+  const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+    return(
+      <select>{comboBoxOpcoes}</select>
+    )
+
+}
+
+
+    const MeuComboBox = () => criaComboBox();
+
+    return(
+
+      <>
+      <input type="text" className="texto-central" value={props.nome} onChange={modificaNome}></input>
+      <h1>Hello {props.nome} e tenho {props.idade} anos </h1>
+      <MeuComboBox />
+      </>
+    )
+  }
+
 
 export default App;
